17e1c2472f7d6e395181fadb1ca9a91a82c8f3c3
水津　快斗 <>
2019-06-18 14:32:08 +0900

    Playerをマウス移動に適応

---
+++ Assets/SuizuKaito/C#/Player/PlayerMouseController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMouseController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject moveobject = null;
+    void Update()
+    {
+        Vector3 touchScreenPosition = Input.mousePosition;
+        touchScreenPosition.z = 10;
+
+        Camera gamecamera = Camera.main;
+        Vector3 touchWorldPosition = gamecamera.ScreenToWorldPoint(touchScreenPosition);
+        moveobject.transform.position = touchWorldPosition;
+    }
+    //void OnMouseDrag()
+    //{
+    //    Vector3 objectPointInScreen =
+    //        Camera.main.WorldToScreenPoint(this.transform.position);
+
+    //    Vector3 mousePointINScreen = new Vector3(Input.mousePosition.x, objectPointInScreen.y, Input.mousePosition.z);
+
+    //    Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointINScreen);
+    //    mousePointInWorld.z = this.transform.position.z;
+    //    this.transform.position = mousePointInWorld;
+    //}
+}
