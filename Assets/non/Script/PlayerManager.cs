dbbdeeade7d7ef8f05ed20d1c8541943d32b380d
ononon <20stss13@gmail.com>
2019-06-25 13:20:55 +0900

    PlayerのUI

---
+++ Assets/non/Script/PlayerManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int Rm;
+    Text text;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        Rm = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "残機:" + Rm;
+    }
+}
